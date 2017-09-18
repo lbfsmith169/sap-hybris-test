cf0409fb7521e7935414b4ec51ec13ac62627f19
Luke Smith <luke.smith@sap.com>
2017-09-18 22:55:49 +0100

    Removed commented out lines

--- src/main/groovy/test/TestPage.groovy
+++ src/main/groovy/test/TestPage.groovy
@@ -15,12 +15,7 @@ class TestPage extends Page {
 
     static content = {
         logInButton(required: true, wait: true) { $("[data-ui-name='login']") }
-        //appSelector(required: true, wait: true) { $("[data-ui-name='select:app']") }
-        //cartRemarketingAppSelector(required: true, wait: true) { $("[data-ui-name='option:cartremarketing']") }
-        //tenantPicker(required: true, wait: true) { $("[data-ui-name='select:tenant']") }
-        //tenantSelector(required: true, wait: true) { $("[data-ui-name='option:Conversion Acceptance Testing Project']") }
         sitesContainer(required: true, wait: true) { $(By.xpath("/html/body/app-root/ng-component/cs-tenant-picker/div"))}
-        //tenantGraphic(wait: true) { $("[data-ui-name='option:Conversion Acceptance Testing Project'] img") }
         success(wait: true) { sitesContainer.isDisplayed() }
     }
 }
